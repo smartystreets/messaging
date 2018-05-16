63ce9fd3a80242c5e9159b488c8ee271a64f632b
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-16 13:56:52 -0600

    Functional options.

--- retry_writer.go
+++ retry_writer.go
@@ -6,17 +6,14 @@ type RetryWriter struct {
 	sleep func(uint64)
 }
 
-func NewRetryWriter(inner Writer, max uint64, sleep func(uint64)) *RetryWriter {
-	if max == 0 {
-		max = 0xFFFFFFFFFFFFFFFF
+func NewRetryWriter(inner Writer, options ...RetryWriterOption) *RetryWriter {
+	this := &RetryWriter{inner: inner, max: 0xFFFFFFFFFFFFFFFF, sleep: func(uint64) {}}
+	for _, option := range options {
+		option(this)
 	}
-
-	return &RetryWriter{
-		inner: inner,
-		max:   max,
-		sleep: sleep,
-	}
+	return this
 }
+
 func (this *RetryWriter) Write(message Dispatch) (err error) {
 	for i := uint64(0); i < this.max; i++ {
 		if err = this.inner.Write(message); err == nil {
@@ -30,7 +27,15 @@ func (this *RetryWriter) Write(message Dispatch) (err error) {
 
 	return err
 }
-
 func (this *RetryWriter) Close() {
 	this.inner.Close()
+}
+
+type RetryWriterOption func(this *RetryWriter)
+
+func WithRetryCallback(callback func(uint64)) RetryWriterOption {
+	return func(this *RetryWriter) { this.sleep = callback }
+}
+func WithMaxAttempts(attempts uint64) RetryWriterOption {
+	return func(this *RetryWriter) { this.max = attempts }
 }
