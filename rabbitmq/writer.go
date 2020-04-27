96c04356e56de1639566b36e44bd8d608be16171
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2020-04-27 14:25:40 -0600

    Implemented rabbit writer.

--- rabbitmq/writer.go
+++ rabbitmq/writer.go
@@ -2,28 +2,92 @@ package rabbitmq
 
 import (
 	"context"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/smartystreets/messaging/v3"
 	"github.com/smartystreets/messaging/v3/rabbitmq/adapter"
+	"github.com/streadway/amqp"
 )
 
 type defaultWriter struct {
+	inner                adapter.Channel
+	panicOnTopologyError bool
+	now                  func() time.Time
 }
 
 func newWriter(inner adapter.Channel, config configuration) messaging.CommitWriter {
-	return defaultWriter{}
+	return defaultWriter{inner: inner, panicOnTopologyError: config.TopologyFailurePanic, now: config.Now}
 }
 
-func (this defaultWriter) Write(ctx context.Context, dispatches ...messaging.Dispatch) (int, error) {
-	panic("implement me")
+func (this defaultWriter) Write(_ context.Context, messages ...messaging.Dispatch) (count int, err error) {
+	now := this.now().UTC()
+
+	for _, message := range messages {
+		count++
+		converted := toAMQPDispatch(message, now)
+		partition := strconv.FormatUint(message.Partition, 10)
+		if err := this.inner.Publish(message.Topic, partition, converted); err != nil {
+			return count - 1, err // writes are async, only channel unavailability causes errors here
+		}
+	}
+
+	return count, nil
 }
+func toAMQPDispatch(dispatch messaging.Dispatch, now time.Time) amqp.Publishing {
+	if dispatch.Timestamp.IsZero() {
+		dispatch.Timestamp = now
+	}
+
+	return amqp.Publishing{
+		AppId:           strconv.FormatUint(dispatch.SourceID, 10),
+		MessageId:       strconv.FormatUint(dispatch.MessageID, 10),
+		CorrelationId:   strconv.FormatUint(dispatch.CorrelationID, 10),
+		Type:            dispatch.MessageType,
+		ContentType:     dispatch.ContentType,
+		ContentEncoding: dispatch.ContentEncoding,
+		Timestamp:       dispatch.Timestamp,
+		Expiration:      computeExpiration(dispatch.Expiration),
+		DeliveryMode:    computePersistence(dispatch.Durable),
+		Body:            dispatch.Payload,
+	}
+}
+func computeExpiration(expiration time.Duration) string {
+	if expiration == 0 {
+		return ""
+	} else if seconds := int(expiration.Seconds()); seconds <= 0 {
+		return "1"
+	} else {
+		return strconv.FormatUint(uint64(seconds), 10)
+	}
+}
+func computePersistence(durable bool) uint8 {
+	if durable {
+		return amqp.Persistent
+	}
+
+	return amqp.Transient
+}
+
 func (this defaultWriter) Commit() error {
-	panic("implement me")
+	return this.tryPanic(this.inner.TxCommit())
 }
 func (this defaultWriter) Rollback() error {
-	panic("implement me")
+	return this.inner.TxRollback()
+}
+func (this defaultWriter) tryPanic(err error) error {
+	if err == nil || !this.panicOnTopologyError {
+		return err
+	}
+
+	if brokerError, ok := err.(*amqp.Error); ok && brokerError.Code == http.StatusNotFound {
+		panic(err)
+	}
+
+	return err
 }
 
 func (this defaultWriter) Close() error {
-	panic("implement me")
+	return this.inner.Close()
 }
---
+++ rabbitmq/writer_test.go
@@ -0,0 +1,225 @@
+package rabbitmq
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+	"github.com/smartystreets/messaging/v3"
+	"github.com/streadway/amqp"
+)
+
+func TestWriterFixture(t *testing.T) {
+	gunit.Run(new(WriterFixture), t)
+}
+
+type WriterFixture struct {
+	*gunit.Fixture
+
+	writer                 messaging.CommitWriter
+	now                    time.Time
+	panicOnTopologyFailure bool
+
+	closeError              error
+	commitError             error
+	rollbackError           error
+	publishError            error
+	publishCallsBeforeError int
+
+	publishExchanges []string
+	publishKeys      []string
+	publishMessages  []amqp.Publishing
+}
+
+func (this *WriterFixture) Setup() {
+	this.initializeWriter()
+}
+func (this *WriterFixture) initializeWriter() {
+	config := configuration{}
+	Options.apply(
+		Options.Now(func() time.Time { return this.now }),
+		Options.PanicOnTopologyError(this.panicOnTopologyFailure),
+	)(&config)
+
+	this.writer = newWriter(this, config)
+}
+
+func (this *WriterFixture) TestWhenCloseInvoked_UnderlyingChannelClosed() {
+	this.closeError = errors.New("")
+
+	err := this.writer.Close()
+
+	this.So(err, should.Equal, this.closeError)
+}
+func (this *WriterFixture) TestWhenRollbackInvoked_UnderlyingChannelRolledBack() {
+	this.rollbackError = errors.New("")
+
+	err := this.writer.Rollback()
+
+	this.So(err, should.Equal, this.rollbackError)
+}
+func (this *WriterFixture) TestWhenCommitInvoke_UnderlyingChannelCommitted() {
+	this.commitError = errors.New("")
+
+	err := this.writer.Commit()
+
+	this.So(err, should.Equal, this.commitError)
+}
+func (this *WriterFixture) TestWhenTopologyNotEstablished_PanicOnTopologyErrors() {
+	this.panicOnTopologyFailure = true
+	this.initializeWriter()
+
+	this.commitError = &amqp.Error{Code: 404}
+
+	this.So(func() { _ = this.writer.Commit() }, should.Panic)
+}
+func (this *WriterFixture) TestWhenTopologyNotEstablished_DontPanicIfIsNotTopologyRelated() {
+	this.panicOnTopologyFailure = true
+	this.initializeWriter()
+
+	this.commitError = errors.New("")
+
+	err := this.writer.Commit()
+
+	this.So(err, should.Equal, this.commitError)
+}
+
+func (this *WriterFixture) TestWhenWrite_PublishToUnderlyingChannel() {
+	count, err := this.writer.Write(context.Background(), messaging.Dispatch{
+		SourceID:        1,
+		MessageID:       2,
+		CorrelationID:   3,
+		Timestamp:       time.Time{},
+		Expiration:      time.Minute,
+		Durable:         true,
+		Topic:           "topic",
+		Partition:       5,
+		MessageType:     "message-type",
+		ContentType:     "content-type",
+		ContentEncoding: "content-encoding",
+		Payload:         []byte("payload"),
+		Headers: map[string]interface{}{
+			"header10": "value10",
+			"header20": int64(20),
+		},
+	})
+
+	this.So(err, should.BeNil)
+	this.So(count, should.Equal, 1)
+
+	this.So(this.publishExchanges, should.Resemble, []string{"topic"})
+	this.So(this.publishKeys, should.Resemble, []string{"5"})
+	this.So(this.publishMessages, should.Resemble, []amqp.Publishing{
+		{
+			Headers:         nil,
+			ContentType:     "content-type",
+			ContentEncoding: "content-encoding",
+			DeliveryMode:    amqp.Persistent,
+			Priority:        0,
+			CorrelationId:   "3",
+			ReplyTo:         "",
+			Expiration:      "60",
+			MessageId:       "2",
+			Timestamp:       time.Time{},
+			Type:            "message-type",
+			UserId:          "",
+			AppId:           "1",
+			Body:            []byte("payload"),
+		},
+	})
+}
+func (this *WriterFixture) TestWhenWriteTransientMessage_PublishTransientMessageToUnderlyingChannel() {
+	const durable = false
+
+	count, err := this.writer.Write(context.Background(), messaging.Dispatch{
+		Durable: durable,
+	})
+
+	this.So(err, should.BeNil)
+	this.So(count, should.Equal, 1)
+
+	this.So(this.publishExchanges, should.Resemble, []string{""})
+	this.So(this.publishKeys, should.Resemble, []string{"0"})
+	this.So(this.publishMessages, should.Resemble, []amqp.Publishing{
+		{
+			MessageId:     "0",
+			CorrelationId: "0",
+			AppId:         "0",
+			DeliveryMode:  amqp.Transient,
+		},
+	})
+}
+func (this *WriterFixture) TestWhenWriteExpirationLessThanOneSecond_UseOneSecondExpiration() {
+	count, err := this.writer.Write(context.Background(), messaging.Dispatch{
+		Expiration: time.Second - 1,
+	})
+
+	this.So(err, should.BeNil)
+	this.So(count, should.Equal, 1)
+
+	this.So(this.publishExchanges, should.Resemble, []string{""})
+	this.So(this.publishKeys, should.Resemble, []string{"0"})
+	this.So(this.publishMessages, should.Resemble, []amqp.Publishing{
+		{
+			MessageId:     "0",
+			CorrelationId: "0",
+			AppId:         "0",
+			DeliveryMode:  amqp.Transient,
+			Expiration:    "1",
+		},
+	})
+}
+func (this *WriterFixture) TestWhenWriterFailsMidwayThrough_ReturnNumberOfWritesThusFarAndError() {
+	this.publishError = errors.New("")
+	this.publishCallsBeforeError = 3
+
+	count, err := this.writer.Write(context.Background(), []messaging.Dispatch{{}, {}, {}}...)
+
+	this.So(count, should.Equal, 2)
+	this.So(err, should.Equal, this.publishError)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func (this *WriterFixture) Close() error      { return this.closeError }
+func (this *WriterFixture) TxCommit() error   { return this.commitError }
+func (this *WriterFixture) TxRollback() error { return this.rollbackError }
+func (this *WriterFixture) Publish(exchange, key string, envelope amqp.Publishing) error {
+	this.publishExchanges = append(this.publishExchanges, exchange)
+	this.publishKeys = append(this.publishKeys, key)
+	this.publishMessages = append(this.publishMessages, envelope)
+
+	if len(this.publishMessages) >= this.publishCallsBeforeError {
+		return this.publishError
+	}
+
+	return nil
+}
+
+func (this *WriterFixture) DeclareQueue(name string) error {
+	panic("implement me")
+}
+func (this *WriterFixture) DeclareExchange(name string) error {
+	panic("implement me")
+}
+func (this *WriterFixture) BindQueue(queue, exchange string) error {
+	panic("implement me")
+}
+func (this *WriterFixture) BufferSize(value uint16) error {
+	panic("implement me")
+}
+func (this *WriterFixture) Consume(consumerID, queue string) (<-chan amqp.Delivery, error) {
+	panic("implement me")
+}
+func (this *WriterFixture) Ack(deliveryTag uint64, multiple bool) error {
+	panic("implement me")
+}
+func (this *WriterFixture) CancelConsumer(consumerID string) error {
+	panic("implement me")
+}
+func (this *WriterFixture) Tx() error {
+	panic("implement me")
+}
